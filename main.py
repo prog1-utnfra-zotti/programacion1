987a90194d5942f5e9f0e7a52cefff7252b4e6c5
Enzo Martin Zotti <e.zotti@sistemas-utnfra.com.ar>
2025-04-28 18:02:54 -0300

    agrego menu

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+from inicio_sesion import iniciar_sesion
+
 print("Bienvenidos a la tienda virtual")
 
 
 
+# Función principal para el menú
+def menu_principal():
+    print("Bienvenido al sistema")
+    
+    if iniciar_sesion():
+        while True:
+            print("\nMenú Principal")
+            print("1. Comprar")
+            print("2. Salir")
+            opcion = input("Selecciona una opción: ")
+            
+            if opcion == "1":
+                pass # aca ira realizar_compra()
+            elif opcion == "2":
+                print("Saliendo del sistema. ¡Hasta luego!")
+                break
+            else:
+                print("Opción no válida. Por favor, intenta nuevamente.")
 
+# Ejecutar el menú principal
+menu_principal()
 
 
 
